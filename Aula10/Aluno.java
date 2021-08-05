c2e65e96a10593701b6ab84fdd6971917fb9e9cc
Adamor Henner <adacandido9@gmail.com>
2021-08-05 12:51:37 -0300

    Herança

---
+++ Aula10/Aluno.java
@@ -0,0 +1,26 @@
+
+public class Aluno extends Pessoa{
+	private int matr;
+	private String curso;
+	
+	public void cancelaMatr() {
+		System.out.println("Matricula cancelada!");
+	}
+
+	public int getMatr() {
+		return matr;
+	}
+	public void setMatr(int matr) {
+		this.matr = matr;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	
+	
+}
